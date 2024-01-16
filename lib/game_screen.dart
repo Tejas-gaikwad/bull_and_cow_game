0883060e428bec3979168cd536fa90f01251d653
Tejas-gaikwad <tejasgaikwad0504@gmail.com>
2024-01-17 00:09:33 +0530

    added clear logic

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -14,6 +14,7 @@ class _GameScreenState extends State<GameScreen> {
   late List<TextEditingController> _controllers;
   int? secretNumber;
   String? feedbackMessage;
+  bool clear = true;
 
   @override
   void initState() {
@@ -35,111 +36,141 @@ class _GameScreenState extends State<GameScreen> {
         backgroundColor: const Color.fromARGB(255, 226, 182, 19),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            feedbackMessage == null
-                ? const SizedBox()
-                : Text(
-                    feedbackMessage.toString(),
-                    style: const TextStyle(
-                      color: Colors.orange,
-                      fontSize: 25,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(height: 20),
+              (feedbackMessage == null || clear)
+                  ? const SizedBox()
+                  : Text(
+                      feedbackMessage.toString(),
+                      style: const TextStyle(
+                        color: Colors.orange,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+              const SizedBox(height: 45),
+              clear
+                  ? const SizedBox()
+                  : Column(
+                      children: [
+                        const Text(
+                          'Generated Number was',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          secretNumber.toString(),
+                          style: const TextStyle(
+                            letterSpacing: 15,
+                            color: Colors.orange,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+              const SizedBox(height: 15),
+              const Text(
+                'Enter Your 4 digits',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(
+                  4,
+                  (index) => SizedBox(
+                    width: 50,
+                    child: TextField(
+                      enabled: clear ? true : false,
+                      focusNode: _focusNodes?[index],
+                      onChanged: (value) {
+                        moveCursor(index);
+                      },
+                      controller: _controllers[index],
+                      keyboardType: TextInputType.number,
+                      maxLength: 1,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 20),
+                      decoration: const InputDecoration(
+                        counterText: '',
+                        hintText: '0',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              InkWell(
+                onTap: () {
+                  checkBullsAndCows();
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: const Color.fromARGB(255, 226, 182, 19),
+                  ),
+                  child: const Text(
+                    'OK',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-            const SizedBox(height: 50),
-            const SizedBox(height: 150),
-            const Text(
-              'Enter Your 4 digits',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                4,
-                (index) => SizedBox(
-                  width: 50,
-                  child: TextField(
-                    focusNode: _focusNodes?[index],
-                    onChanged: (value) {
-                      moveCursor(index);
-                    },
-                    controller: _controllers[index],
-                    keyboardType: TextInputType.number,
-                    maxLength: 1,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 20),
-                    decoration: const InputDecoration(
-                      counterText: '',
-                      hintText: '0',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            InkWell(
-              onTap: () {
-                checkBullsAndCows();
-              },
-              child: Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width / 1.2,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: const Color.fromARGB(255, 226, 182, 19),
-                ),
-                child: const Text(
-                  'OK',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            InkWell(
-              onTap: () {
-                for (var controller in _controllers) {
-                  controller.clear();
-                }
-                setState(() {});
-              },
-              child: Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width / 2,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: Colors.transparent,
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 226, 182, 19),
-                  ),
-                ),
-                child: const Text(
-                  'Clear',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 226, 182, 19),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            )
-          ],
+              const SizedBox(height: 30),
+              clear
+                  ? const SizedBox()
+                  : InkWell(
+                      onTap: () {
+                        for (var controller in _controllers) {
+                          controller.clear();
+                        }
+                        clear = true;
+                        setState(() {});
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width / 2,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Colors.transparent,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 226, 182, 19),
+                          ),
+                        ),
+                        child: const Text(
+                          'Clear',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 226, 182, 19),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+            ],
+          ),
         ),
       ),
     );
@@ -162,6 +193,7 @@ class _GameScreenState extends State<GameScreen> {
       }
     }
     setState(() {
+      clear = false;
       feedbackMessage = 'Bulls: $bulls, Cows: $cows';
     });
   }
